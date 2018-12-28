94682e0070fd29b1dda90a7e5b833720de0df595
maaahad <ahad3112@yahoo.com>
2018-12-28 23:57:19 +0100

    RMContainer:: map test

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <map>
 #include "CGContainer.hpp"
 #include "B.hpp"
 #include "G.hpp"
@@ -13,13 +14,24 @@ int main() {
 
     std::vector<CGContainerBase *> list = {cg1, cg2, cg3, cg4};
 
+    std::shared_ptr<std::map<CG_TYPE , CGContainerBase*>> list2 = std::make_shared<std::map<CG_TYPE , CGContainerBase*>>();
+    list2->insert(std::make_pair(CG_TYPE::WIFI, cg1));
+    list2->insert(std::make_pair(CG_TYPE::WIFI, cg2));
+    list2->insert(std::make_pair(CG_TYPE::INTERNAL_ERROR, cg3));
+    list2->insert(std::make_pair(CG_TYPE::OTHER, cg4));
 
+    std::cout << "List2: " << std::endl;
     for (auto it = list.cbegin(); it != list.cend(); it++){
         //auto cg = static_cast<const CGContainer<B,G,std::string> *>(*it);
         //std::cout << "CGContainer : " << *cg << std::endl;
         //std::cout << (*(*it)) << std::endl;
         (*it)->evaluate();
 
+    }
+
+    std::cout << "List2: " << std::endl;
+    for (auto it = list2->cbegin(); it != list2->cend(); it++){
+        ((*it).second)->evaluate();
     }
 
     return 0;
